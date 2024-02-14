41830267c523eff82e7e5bb6d41b02cfb21890c9
yudavid42 <david06@42maru.ai>
2024-02-14 10:50:04 +0900

    sockets.py 코드 수정

--- sockets.py
+++ sockets.py
@@ -1,7 +1,7 @@
 import socketio
 from collections import defaultdict
 
-user_room = defaultdict(list)
+user_room = defaultdict(str)
 
 # create a Socket.IO server
 sio_server = socketio.AsyncServer(
@@ -15,13 +15,13 @@ sio_app = socketio.ASGIApp(
     socketio_path='sockets')
 
 
-# 연결 시
+# 연결
 @sio_server.on('connect')
 async def connect(sid, env):
     print(f'{sid} 연결')
 
 
-# 연결 해제 시
+# 연결 해제
 @sio_server.on('disconnect')
 async def disconnect(sid):
     # 채팅방 퇴장
@@ -46,7 +46,7 @@ async def enter_room(sid, data: dict):
     user_room[sid] = data['room']
 
 
-# 클라이언트가 메시지 전송 시
+# 클라이언트가 메시지 전송
 @sio_server.on('sendMessage')
 async def get_message(sid, data: dict):
     print(f'메시지를 전달 받았습니다. | {data}')
